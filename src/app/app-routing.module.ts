c67f271e07efd9dda7972054182229bd56bcd1b0
Christian Pillaga <christian_pillaga_s@hotmail.es>
2020-11-26 02:06:44 -0500

    inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AguaComponent } from './pages/serviciosB/agua/agua.component';
+import { LuzComponent } from './pages/serviciosB/luz/luz.component';
+import { TelefonoComponent } from './pages/serviciosB/telefono/telefono.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'servicios/agua', component: AguaComponent },
+  { path: 'servicios/luz', component: LuzComponent },
+  { path: 'servicios/telefono', component: TelefonoComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AguaComponent } from './pages/serviciosB/agua/agua.component';
+import { LuzComponent } from './pages/serviciosB/luz/luz.component';
+import { TelefonoComponent } from './pages/serviciosB/telefono/telefono.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    InicioComponent,
+    AguaComponent,
+    LuzComponent,
+    TelefonoComponent
   ],
   imports: [
     BrowserModule,
